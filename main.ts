2755d9be6522679cc8412796932d40074817701f
nikolabaric2014 <76596187+nikolabaric2014@users.noreply.github.com>
2020-12-28 10:01:30 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,19 +5,19 @@ maqueen.IR_callbackUser(function ({ myparam: message }) {
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
     }
     if (message == 17) {
-        item.showColor(neopixel.colors(NeoPixelColors.Orange))
+        item.showColor(neopixel.colors(NeoPixelColors.White))
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 150)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 150)
     }
     if (message == 16) {
         item.showColor(neopixel.colors(NeoPixelColors.Green))
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 20)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
     }
     if (message == 18) {
         item.showColor(neopixel.colors(NeoPixelColors.Green))
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 20)
     }
     if (message == 19) {
         item.showColor(neopixel.colors(NeoPixelColors.Red))
@@ -31,3 +31,9 @@ maqueen.IR_callbackUser(function ({ myparam: message }) {
 })
 let item: neopixel.Strip = null
 item = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (maqueen.sensor(PingUnit.Centimeters) < 20 && maqueen.sensor(PingUnit.Centimeters) != 0) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+    }
+})

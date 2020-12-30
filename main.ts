e20fdeda4e856c0663f9dfdd076db595651bd6cd
nikolabaric2014 <76596187+nikolabaric2014@users.noreply.github.com>
2020-12-30 18:56:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,6 @@ maqueen.IR_callbackUser(function ({ myparam: message }) {
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 150)
     }
     if (message == 16) {
-        item.showColor(neopixel.colors(NeoPixelColors.Green))
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 20)
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
     }
@@ -21,8 +20,7 @@ maqueen.IR_callbackUser(function ({ myparam: message }) {
         maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
     }
 })
-let item: neopixel.Strip = null
-item = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+let item = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     if (maqueen.sensor(PingUnit.Centimeters) < 20 && maqueen.sensor(PingUnit.Centimeters) != 0) {
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
